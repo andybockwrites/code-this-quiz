fccde82703cf362318e65b207b5bdedb3e416954
andybockwrites <andybockwrites@gmail.com>
2023-01-26 00:25:03 -0500

    made some progress, but I may need to rethink my approach.

--- assets/script.js
+++ assets/script.js
@@ -3,103 +3,102 @@ var tickingTimerEl = $('#time-left');
 var quizBoxContainerEl = $('.quiz-box');
 var questionsElement = $('#questions');
 var answersElement = $('#answers');
-var ResultsEl = $('#correct-wrong');
+var answerOne = $('#A');
+var answerTwo = $('#B');
+var answerThree = $('#C');
+var answerFour = $('#D');
+var resultsEl = $('#correct-wrong');
 
-var question0 = ("In California, you can't legally buy a mousetrap without having what? (a hunting license)");
-var question1 = ("In Minnesota, it is illegal to tease what type of animal? (skunks)");
-var question2 = ("According to those close to him, what was Walt Disney truly afraid of? (mice)");
-var question3 = ("What is illegal to eat with a cherry pie in Kansas? (ice cream)");
-var question4 = ("Which state passed a law in 1961 making it illegal to eat fried chicken with a fork? (Georgia)");
+var quizData = [
+    {
+        question:"In California, you can't legally buy a mousetrap without having what? (a hunting license)",
+        A:'Written approval by the governor',
+        B:'A hunting license',
+        C: 'Vegan Cheese',
+        D: 'Photograph proof of the mice',
+        correct: "B"
+    },
+    {
+        question:"In Minnesota, it is illegal to tease what type of animal? (skunks)",
+        A:'Skunks',
+        B:'Bears',
+        C: 'Foxes',
+        D: 'Dogs',
+        correct: "A"
+    },
+    {
+        question:"According to those close to him, what was Walt Disney truly afraid of? (mice)",
+        A:'Spiders',
+        B:'Artistic Theft',
+        C: 'Lightning',
+        D: 'Mice',
+        correct: "D"
+    },
+    {
+        question:"What is illegal to eat with a cherry pie in Kansas? (ice cream)",
+        A:'Any entrees',
+        B:'Key Lime Pie',
+        C: 'Ice Cream',
+        D: 'Fresh Cherries',
+        correct: "C"
+    },
+    {
+        question:"Which state passed a law in 1961 making it illegal to eat fried chicken with a fork? (Georgia)",
+        A:'Louisiana',
+        B:'Alabama',
+        C: 'Kentucky',
+        D: 'Georgia',
+        correct: "D"
+    }
+];
+console.log(quizData)
 
-var answers1 = ['Written approval by the governor', 'A hunting license', 'Vegan Cheese', 'Photograph proof of the mice'];
-var answers2 = ['Skunks', 'Bears', "Foxes", "Dogs"];
-var answer3 = ['Spiders', 'Artistic Theft', 'Lightning', 'Mice'];
-var answer4 = ['Any entrees', 'Key Lime Pie', 'Ice Cream', 'Fresh Cherries'];
-var answer5 = ['Louisiana', 'Alabama', 'Kentucky', 'Georgia'];
 
+var points = 0;
+var currentQuiz = 0;
 
+loadQuiz();
+function loadQuiz() {
+    var currentQuizData = quizData[currentQuiz];
+    console.log(currentQuizData);
+    questionsElement.text(currentQuizData.question);
+    console.log(quizData[0].A)
+    answerOne.text(quizData[0].A);
+    answerTwo.text(quizData[0].B);
+    answerThree.text(quizData[0].C);
+    answerFour.text(quizData[0].D);
+    // console.log(quizData[0].A)
+    if (answerTwo.click == true){
+        resultsEl = resultsEl.text("Congratulations! That's correct");
+        points ++;
+        secondQuestion();
+    } else if (answerOne.click == true|| answerThree.click == true|| answerFour.click == true) {
+        resultsEl.text("Sorry! That's incorrect");
+        secondQuestion();
+    }
 
-var question0Funct = function() {
-    questionsElement(question0).text($(question0));
-    console.log(question0);
-    answersElement[0].append.text($(answers1[0]));
-    answersElement[1].append.text($(answers1[1]));
-    answersElement[2].append.text($(answers1[2]));
-    answersElement[3].append.text($(answers1[3]));
-    if (
-        (questionsElement[0] === true) ||
-        (answersElement[1].input.value === true)) {
-           ResultsEl.text("That's Correct!")
-        } else {
-            ResultsEl.text("Sorry! That's incorrect.") 
-        }
+function secondQuestion() {
+    var currentQuizData = quizData[currentQuiz];
+    console.log(currentQuizData);
+    questionsElement.text(currentQuizData.question);
+    console.log(quizData[1].A)
+    answerOne.text(quizData[1].A);
+    answerTwo.text(quizData[1].B);
+    answerThree.text(quizData[1].C);
+    answerFour.text(quizData[1].D);
+    // console.log(quizData[0].A)
+    if (answerTwo.click == true){
+        resultsEl = resultsEl.text("Congratulations! That's correct");
+        points ++;
+    } else (answerOne.click == true|| answerThree.click == true|| answerFour.click == true)
+        resultsEl.text("Sorry! That's incorrect");
 }
+    
 
-var question1Funct = function() {
-    questionsElement(question1).text(question1);
-    answersElement[0].append.text($(answers1[0]));
-    answersElement[1].append.text($(answers1[1]));
-    answersElement[2].append.text($(answers1[2]));
-    answersElement[3].append.text($(answers1[3]));
-    if (
-        (questionsElement[1] === true) ||
-        (answersElement[0].input.value === true)) {
-           ResultsEl.text("That's Correct!")
-        } else {
-            ResultsEl.text("Sorry! That's incorrect.") 
-        }
+
+    /*for (var i = 1; i > quizData; i++) {
+
+    } */
 }
+/* Creat Li to place answers in (quizData). Put inside of a loop. */
 
-var question2Funct = function() {
-    questionsElement(question2).text(question2);
-    answersElement[0].append.text($(answers1[0]));
-    answersElement[1].append.text($(answers1[1]));
-    answersElement[2].append.text($(answers1[2]));
-    answersElement[3].append.text($(answers1[3]));
-    if (
-        (questionsElement[2] === true) ||
-        (answersElement[1].input.value === true)) {
-           ResultsEl.text("That's Correct!")
-        } else {
-            ResultsEl.text("Sorry! That's incorrect.") 
-        }
-}
-
-var question3Funct = function() {
-    questionsElement(question4).text(question3);
-    answersElement[0].append.text($(answers1[0]));
-    answersElement[1].append.text($(answers1[1]));
-    answersElement[3].append.text($(answers1[2]));
-    answersElement[4].append.text($(answers1[3]));
-    if (
-        (questionsElement[3] === true) ||
-        (answersElement[3].input.value === true)) {
-           ResultsEl.text("That's Correct!")
-        } else {
-            ResultsEl.text("Sorry! That's incorrect.") 
-        }
-}
-
-var question4Funct = function() {
-    questionsElement(question5).text(question4);
-    answersElement[0].append.text($(answers1[0]));
-    answersElement[1].append.text($(answers1[1]));
-    answersElement[3].append.text($(answers1[2]));
-    answersElement[4].append.text($(answers1[3]));
-    if (
-        (questionsElement[4] === true) ||
-        (answersElement[3].input.value === true)) {
-           ResultsEl.text("That's Correct!")
-        } else {
-            ResultsEl.text("Sorry! That's incorrect.") 
-        }
-}
-
-
-document.addEventListener('click', question0Funct());
-
-question0Funct();
-question1Funct();
-question2Funct();
-question3Funct();
-question4Funct();
